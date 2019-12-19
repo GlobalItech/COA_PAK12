23824b91526c7e936b88a5fabf38ab29d91adbaa
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2019-12-19 16:14:02 +0500

    Update __manifest__.py

--- COA_pak/__manifest__.py
+++ COA_pak/__manifest__.py
@@ -24,7 +24,7 @@
     'author': 'Itech Resources,Steigend IT Solutions',
     'website': 'http://itechresources.net/',
     'category': 'Accounting &amp; Finance',
-    'version': '1.2.1',
+    'version': '12.2.1',
     'depends': ['base', 'account'],
     'data': [
         'security/account_parent_security.xml',
